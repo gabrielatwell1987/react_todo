c8d19146cdee10fca0e9622c2342cc46aeb4c7a0
gabrielatwell1987 <gabrielatwell@proton.me>
2025-02-13 11:41:05 -0800

    fixed the way the github link looks for mobile.. made it fixed

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import Link from "./components/link/Link";
 
 function App() {
   return (
-    <div className="App">
-      <TodoWrapper />
-
+    <>
+      <div className="App">
+        <TodoWrapper />
+      </div>
       <Link />
-    </div>
+    </>
   );
 }
 
